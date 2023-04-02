a1f1d3618bc4d8a0ab1e36d68894f99229bee434
Samuel Marks <807580+SamuelMarks@users.noreply.github.com>
2023-04-01 23:49:20 -0400

    [index.html] Use markmap-autoloader instead of more custom solution ; [build.js] Update to use script tags as used by markmap-autoloader

--- build.js
+++ build.js
@@ -4,16 +4,20 @@ const { readFile, writeFile } = require('fs');
 const { join } = require('path');
 
 
+const script_start = '<script type="text/template">';
+const script_end = '</script>';
+
 if (require.main === module) {
     const html_filename = join(__dirname, 'index.html');
     readFile(join(__dirname, '180 companies.md'), 'utf8', (err, markdown) => {
         if (err != null) throw err;
         readFile(html_filename, 'utf8', (er, html) => {
             if (er != null) throw er;
-            const fst = html.indexOf('`');
-            const snd = html.indexOf('`', fst+1);
+            /* could us jsdom instead */
+            const fst = html.indexOf(script_start) +script_start.length;
+            const snd = html.indexOf(script_end, fst);
             writeFile(html_filename,
-                      `${html.substring(0, fst)}\`${markdown}\`${html.substring(snd+1)}`,
+                      `${html.substring(0, fst)}\n${markdown}${html.substring(snd)}`,
                       'utf8', (e) => { if (e != null) throw e; });
         });
     });
